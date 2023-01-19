9f8bd7f31b8fc4e19f2069f0a6330cdb286186bd
Tee O <tdopress@gmail.com>
2023-01-19 17:53:47 -0500

    added document hook to facilitate checking for template requirements

--- client/src/components/ProtectedRoute.jsx
+++ client/src/components/ProtectedRoute.jsx
@@ -1,15 +1,19 @@
 import React from 'react'
 import { Navigate } from 'react-router-dom'
 import { useAuth } from './hooks/userAuth'
+import { useDocument } from './hooks/document'
 
 const ProtectedRoute = ({children, required}) => {
     const auth = useAuth()
+    const document = useDocument()
     const operations = {
         login: () => {
             if (!auth.user) return <Navigate to='/login' />
         },
         //TODO: implement global context to store template currently being edited
-        template: () => {}
+        template: () => {
+            if (!document.editing) return <Navigate to='/new' />
+        }
     }
     if (operations[required]) operations[required]();
   return children
