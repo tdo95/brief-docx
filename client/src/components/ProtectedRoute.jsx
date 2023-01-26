3b4975ca741c996d3d618c485ee5861145544dd7
Tee O <tdopress@gmail.com>
2023-01-25 22:38:29 -0500

    added functionality to verify user

--- client/src/components/ProtectedRoute.jsx
+++ client/src/components/ProtectedRoute.jsx
@@ -1,11 +1,23 @@
-import React from 'react'
+import { React, useEffect, useState } from 'react'
 import { Navigate } from 'react-router-dom'
 import { useAuth } from './hooks/auth'
 import { useDocument } from './hooks/document'
 
 const ProtectedRoute = ({children, required}) => {
+    
     const auth = useAuth()
+    console.log('Protected user:', auth)
     const document = useDocument()
+    const [loading, setLoading] = useState(true)
+    console.log('protected route rendering, loading:', loading)
+
+    useEffect(() => {
+        //Verify user 
+        auth.getUser()
+            .then(() => setLoading(false))
+            .catch(err => console.log(err))
+    }, [])
+
     //returns new nagivation route if required operation is denied
     const operations = {
         login: () => {
@@ -16,7 +28,10 @@ const ProtectedRoute = ({children, required}) => {
             if (!document.editing) return <Navigate to='/new' />
         }
     }
-  return operations[required]() || children;
+
+  //TODO: Change this to a loading indicator
+  if (loading) return <div>Loading.....</div>
+  else return operations[required]() || children;
 }
 
 export default ProtectedRoute
