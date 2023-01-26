8d0146b9998f524bc49809709940e9eed7dbfb6d
Tee O <tdopress@gmail.com>
2023-01-25 22:36:55 -0500

    configured error handling and form submission

--- client/src/components/Auth.jsx
+++ client/src/components/Auth.jsx
@@ -1,29 +1,84 @@
-import { React, useState } from 'react'
-import { Navigate } from 'react-router-dom';
+import { React, useState, useEffect } from 'react'
+import { Navigate, redirect, useLocation } from 'react-router-dom';
 import { Container, Box, Typography, TextField, Button } from '@mui/material'
 import { useAuth } from './hooks/auth';
 
 const Auth = ({ login }) => {
     const auth = useAuth()
+    const location = useLocation()
+    //Each property has an underscore in order to avoid browser detection of input feild and autofilling values
     const [authForm, setAuthForm] = useState({
         _name: '',
         _password: '',
         _passwordConfirm: '',
         _email: '' 
     })
+    const [error, setError] = useState({
+        name: '',
+        email: '',
+        password: '',
+        passwordConfirm: ''
+    })
+    //cleanup inputs on location change 
+    useEffect(() => {
+        console.log('cleaning up inputs')
+        setAuthForm({
+            _name: '',
+            _password: '',
+            _passwordConfirm: '',
+            _email: '' 
+        })
+        setError({
+            name: '',
+            email: '',
+            password: '',
+            passwordConfirm: ''
+        })
+    }, [location])
     const handleFormInput = (event) => setAuthForm(prev => ({
         ...prev,
         [event.target.name]: event.target.value
     }))
+    //TODO: Refractor login and signup into one function
     const submitForm = (event) => {
         event.preventDefault()
         console.log('Submitting form! (:')
-        //TODO: Add logic to submit form
-        if (login) auth.login(authForm['_email'], authForm['_password']);
+        
+        if (login) 
+            loginUser()
+        else 
+            signupUser()
+    }
+    const loginUser = async () => {
+        const res = await auth.login(authForm['_email'], authForm['_password']);
+        console.log('auth component res', res)
+        if (res?.errors) setError(() => ({
+            email: '',
+            password: '',
+            ...res.errors
+        }))
+        //if no errors, redirect to dashboard
+        else if (res.success) redirect('/')
+
+    }
+    const signupUser = async () => {
+        const res = await auth.signup(authForm)
+        console.log(res)
+        if (res?.errors) setError(prev => ({
+            name: '',
+            email: '',
+            password: '',
+            passwordConfirm: '',
+            ...res.errors
+        }))
+        //if no errors, redirect to dashboard
+        else if (res.success) return redirect('/')
     }
     
     //Redirects from login page to dashbaord if user is logged in
-    if (login && auth.user) { return <Navigate to='/'/>}
+    if (login && auth.user)  return <Navigate to='/' />
+
+    
     
     return (
     <Container sx={{pt:3, display: 'flex', flexDirection:'column', alignItems:'center'}}>
@@ -36,8 +91,10 @@ const Auth = ({ login }) => {
                id='outlined-input'
                value={authForm['_email']}
                onChange={handleFormInput}
-               
+               helperText={error.email}
+               error={Boolean(error.email)}
             />
+            {/* TODO: Hide password text somehow without the browser detecting its a password input feild lol idk if that's possible */}
             <TextField 
                required
                label='password'
@@ -45,6 +102,8 @@ const Auth = ({ login }) => {
                id='outlined-input'
                value={authForm['_password']}
                onChange={handleFormInput}
+               helperText={error.password}
+               error={Boolean(error.password)}
             />
         </Box> :
         <Box autoComplete='off' component='form' sx={{display: 'flex', flexDirection:'column', alignItems:'center', p: 2, '& > *.MuiFormControl-root': {m:'10px', width: '25ch'}}}>
@@ -55,6 +114,8 @@ const Auth = ({ login }) => {
                id='outlined-input'
                value={authForm['_name']}
                onChange={handleFormInput}
+               helperText={error.name}
+               error={Boolean(error.name)}
             />
             <TextField 
                required
@@ -63,6 +124,8 @@ const Auth = ({ login }) => {
                id='outlined-input'
                value={authForm['_email']}
                onChange={handleFormInput}
+               helperText={error.email}
+               error={Boolean(error.email)}
             />
             <TextField 
                required
@@ -71,6 +134,8 @@ const Auth = ({ login }) => {
                id='outlined-input'
                value={authForm['_password']}
                onChange={handleFormInput}
+               helperText={error.password}
+               error={Boolean(error.password)}
             />
             <TextField 
                required
@@ -79,6 +144,8 @@ const Auth = ({ login }) => {
                id='outlined-input'
                value={authForm['_passwordConfirm']}
                onChange={handleFormInput}
+               helperText={error.passwordConfirm}
+               error={Boolean(error.passwordConfirm)}
             />
         </Box>}
         <Button variant='contained' onClick={submitForm}>{login ? 'Log In' : 'Sign Up' }</Button>
