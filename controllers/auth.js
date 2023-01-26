7789a61afebace78fc624539d1f8f4a61ec20092
Tee O <tdopress@gmail.com>
2023-01-26 12:58:16 -0500

    edited logout controller error and success messages

--- controllers/auth.js
+++ controllers/auth.js
@@ -44,10 +44,12 @@ exports.logout = (req, res) => {
     console.log('User has logged out.')
   })
   req.session.destroy((err) => {
-    if (err)
+    if (err) {
       console.log("Error : Failed to destroy the session during logout.", err);
+      res.send({errors: 'Failed to destroy the session during logout.'});
+    }
     req.user = null;
-    res.send({success: 'User logged'});
+    res.send({success: 'User logged out'});
   });
 };
 
