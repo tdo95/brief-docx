4a8f38b333ce50440635e815e3e8c84d33e4cef0
Tee O <tdopress@gmail.com>
2023-02-10 14:12:19 -0500

    implemented functionality to block navigation

--- client/src/components/CreateDocument.jsx
+++ client/src/components/CreateDocument.jsx
@@ -1,6 +1,23 @@
-import React from 'react'
+import { React, useCallback } from 'react'
+import { useBlocker } from './hooks/prompt.blocker'
+import { useDocument } from './context/document'
+
 
 const CreateDocument = () => {
+    const onLeavePrompt = `Are you sure you want to leave?\nAny changes not yet saved will be lost.`;
+    const document = useDocument();
+
+   //Prompts user to confirm navigation and sets editing to false if user chooses to navigate away
+    const confirmNavigation = (tx) => {
+        if ( window.confirm( onLeavePrompt ) ) { 
+            //Set editing context to false
+            document.setEditing(false);
+            tx.retry();
+        }
+        
+    }
+    useBlocker(useCallback(confirmNavigation, []))
+
   return (
     <div>CreateDocument</div>
   )
