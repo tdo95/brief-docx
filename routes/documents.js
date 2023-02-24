86b441e7a5ab771eefb61e64fdb711b1242c0774
Tee O <tdopress@gmail.com>
2023-02-24 14:26:59 -0500

    add route paramter for retrieving user documents

--- routes/documents.js
+++ routes/documents.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const documentsController = require("../controllers/documents");
 
 //Document Routes - simplified for now
-router.get("/", documentsController.getDocuments);
+router.get("/:userId", documentsController.getDocuments);
 router.get("/template/:name", documentsController.getTemplate);
 router.get("/generate/:template", documentsController.generateDoc);
 router.post("/new", documentsController.createDocument);
