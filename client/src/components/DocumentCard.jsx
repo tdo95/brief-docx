e78fc557bebb3b9cbc82e3b91606ff7b1ff7202e
Tee O <tdopress@gmail.com>
2023-02-24 14:24:00 -0500

    add document thumb card component

---
+++ client/src/components/DocumentCard.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import { Card } from '@mui/material'
+const DocumentCard = ({ info }) => {
+  return (
+    <Card sx={{width: 120, minHeight: 150, m:1}}></Card>
+  )
+}
+
+export default DocumentCard
