401ee69953fc8629281f6a376fcb27c07f0fe701
Tee O <tdopress@gmail.com>
2023-02-28 14:33:56 -0500

    implement doc title editing & add error handling

--- client/src/components/CreateDocument.jsx
+++ client/src/components/CreateDocument.jsx
@@ -1,4 +1,6 @@
 import { React, useCallback, useState } from 'react'
+import { TextField, IconButton, Stack, Alert } from '@mui/material'
+import EditIcon from '@mui/icons-material/Edit';
 import { useBlocker } from './hooks/prompt.blocker'
 import { useDocument } from './context/document'
 import { saveAs } from 'file-saver'
@@ -8,9 +10,58 @@ import 'react-pdf/dist/esm/Page/TextLayer.css';
 import testPDF from './test/outpdf.pdf'
 
 const CreateDocument = () => {
+    const onLeavePrompt = `Are you sure you want to leave?\nAny changes not yet saved will be lost.`;
+    const document = useDocument();  
     const [pdf, setPdf] = useState(null);
     const [numPages, setNumPages] = useState(null);
     const [pageNumber, setPageNumber] = useState(1);
+    const [error, setError] = useState(null);
+    const [editingTitle, setEditingTitle] = useState(false);
+    
+    const [documentForm, setDocumentForm] = useState({
+      documentTitle: document.editing.title,
+      shortDocTitle: document.editing.title.slice(0,30) + '...',
+      storyTitle: '',
+      storyDescription: '',
+      storySource: '',
+      storyDate: '',
+      storyLink: '',
+    });
+    const handleForm = (e) => {
+      const { value, name } = e.target;
+      console.log(value)
+      setDocumentForm(prev => {
+        const updateForm = {...prev};
+        updateForm[name] = value;
+        if (name === 'documentTitle') {
+          updateForm['shortDocTitle'] = value.slice(0,30) + '...'
+        }
+        return updateForm
+      })
+    }
+    
+    const updateDoc = async (info) => {
+      console.log('updating document', info)
+      const res = await document.updateDocument(info);
+      const data = await res.json()
+      if (data.error) setError('Oops! Looks like we couldn\'t save your changes. Please try again later.')
+
+    }
+    const saveInput = () => {
+      console.log('User has clicked away!')
+      //Set placeholder if input is empty
+      if(!documentForm.documentTitle) {
+        setDocumentForm(prev => ({
+          ...prev,
+          documentTitle: 'Untitled'
+        }))
+      }
+      //Update title in database
+      updateDoc({title: documentForm.documentTitle})
+
+      //Set editing to false
+      setEditingTitle(false)
+    }
     function onDocumentLoadSuccess({ numPages }) {
         setNumPages(numPages);
       }
@@ -26,8 +77,7 @@ const CreateDocument = () => {
             setPdf(event.target.result)
         }
     }
-    const onLeavePrompt = `Are you sure you want to leave?\nAny changes not yet saved will be lost.`;
-    const document = useDocument();
+    
 
    //Prompts user to confirm navigation and sets editing to false if user chooses to navigate away
     const confirmNavigation = (tx) => {
@@ -42,7 +92,39 @@ const CreateDocument = () => {
 
   return (
     <div className='docContainer'>
-        <Document file={pdf} onLoadSuccess={onDocumentLoadSuccess}>
+      {error && <Alert severity='error'>{error}</Alert>}
+      <Stack sx={{alignItems: 'center', flexDirection:'row'}}>
+        <TextField
+          sx={{
+            '.Mui-disabled:not(input), .Mui-disabled > *:not(input)': {
+              color: 'transparent !important',
+              'border-color': 'transparent !important',
+              
+            },
+            'input.Mui-disabled, input': {
+              'font-size':'30px !important',
+              '-webkit-text-fill-color': 'black',
+            },
+            width: '50%'
+          }}
+          id='title'
+          disabled={!editingTitle}
+          name='documentTitle'
+          variant='outlined'
+          size='small'
+          label='Title'
+          value={ editingTitle || documentForm.documentTitle.length < 30 ? documentForm.documentTitle :  documentForm.shortDocTitle}
+          onChange={handleForm}
+          helperText={!(documentForm.documentTitle) && editingTitle &&'Please enter a title.'}
+          onBlur={saveInput}
+          error={!(documentForm.documentTitle) && editingTitle}
+          inputRef={input => input && input.focus()}>
+        </TextField>
+        {!editingTitle && <IconButton aria-label='save' onClick={() => setEditingTitle(prev => !prev)}>
+          <EditIcon />
+        </IconButton>}
+      </Stack>
+      <Document file={pdf} onLoadSuccess={onDocumentLoadSuccess}>
         {Array.from(new Array(numPages), (el, index) => (
               <Page key={`page_${index + 1}`} pageNumber={index + 1} />
         ))}
