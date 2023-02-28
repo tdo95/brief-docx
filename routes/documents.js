bddfcd850042137a639a0781a551c8d8416987cb
Tee O <tdopress@gmail.com>
2023-02-28 14:37:04 -0500

    add document put, delete routes

--- routes/documents.js
+++ routes/documents.js
@@ -4,8 +4,12 @@ const documentsController = require("../controllers/documents");
 
 //Document Routes - simplified for now
 router.get("/:userId", documentsController.getDocuments);
-router.get("/template/:name", documentsController.getTemplate);
-router.get("/generate/:template", documentsController.generateDoc);
 router.post("/new", documentsController.createDocument);
+router.put("/update/:docId", documentsController.updateDocument);
+router.delete("/delete/:docId", documentsController.deleteDocument);
+
+router.get("/template/:name", documentsController.getTemplate);
+router.get("/generate/:template", documentsController.generateDocument);
+
 
 module.exports = router;
