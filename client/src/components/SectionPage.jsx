27c125eebda5863e153b2a5a3ba8fbeec887c2c7
Tee O <tdopress@gmail.com>
2023-03-09 18:42:43 -0500

    fix formatting

--- client/src/components/SectionPage.jsx
+++ client/src/components/SectionPage.jsx
@@ -1,11 +1,16 @@
 import {React, useState} from 'react'
 import { Button } from '@mui/material'
 import Form from './Form'
-const SectionPage = ({sectionName, sectionStories}) => {
+const SectionPage = ({sectionName, sectionSummary}) => {
     const [toggleForm, setToggleForm] = useState(false)
   return (
     <div>
-        <Button variant='contained' onClick={() => setToggleForm(prev => !prev)}>{toggleForm ? 'X' : 'Add Story'}</Button>
+        <Button 
+          variant='contained' 
+          onClick={() => setToggleForm(prev => !prev)}
+        >       
+          {toggleForm ? 'X' : 'Add Story'}
+        </Button>
         { toggleForm && <Form section={sectionName} /> }
     </div>
   )
