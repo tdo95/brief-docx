a6a92c643f3e9ee3d189caafd9236df6f0a99938
Tee O <tdopress@gmail.com>
2023-03-09 18:43:53 -0500

    add success and error handling

--- client/src/components/Form.jsx
+++ client/src/components/Form.jsx
@@ -1,9 +1,15 @@
-import { React, useState } from 'react'
+import { React, useState, useEffect } from 'react'
 import { TextField, Box, Button, Alert } from '@mui/material'
+import { useDocument } from './context/document'
 
 const Form = ({ section }) => {
+    const document = useDocument();
     const today = new Date();
-    const [error, setError] = useState(null)
+    const [alert, setAlert] = useState(false)
+    const [message, setMessage] = useState({
+        error: '',
+        success: ''
+    })
     const [form, setForm] = useState({
         title: '',
         link: '',
@@ -14,7 +20,6 @@ const Form = ({ section }) => {
     })
     const handleForm = (e) => {
         const { name, value } = e.target;
-        console.log(form, name, value)
         setForm(prev => {
             const newForm = {...prev}
             newForm[name] = value;
@@ -27,17 +32,53 @@ const Form = ({ section }) => {
             return newForm
         })
     }
-    const saveForm = () => {
+    const saveForm = async () => {
         //Check if any form values are empty
-        if(Object.values(form).some(value => !value))
-            return setError('Please fill out each feild in the form')
-        //TODO: Save summary in database
-        //Reset error
-        setError('')
+        if(Object.values(form).some(value => !value)){
+            //Note: Im using the alert state variable as a trigger for the use effect to clear the alert after a few seconds, there it probably a better way to go about this, but this will do for now.
+            setAlert(prev => !prev)
+            setMessage({success: '', error: 'Please fill out each feild in the form' })
+            return;
+        }
+        //TODO: Check that link adheres to 'https://' format
+        //Save summary in database
+        const res = await document.addSummary({
+            title: form.title,
+            description: form.description,
+            date: form.dateFormatted,
+            source: form.source,
+            link: form.link,
+            section: section,
+        })
+        if (res.error) {
+            setAlert(prev => !prev)
+            setMessage({success: '', error: 'Opps. An error occured while trying to save your summary, please try again later.' })
+        }
+        else if (res.summary) {
+            setAlert(prev => !prev)
+            setMessage({error: '', success: 'Success! Summary has been added' })
+            //clear form
+            setForm(prev => ({
+                ...prev,
+                title: '',
+                link: '',
+                description: '',
+                source: '',
+            }))
+        }  
+        
     }
+    useEffect(() => {
+        console.log('Timer going boi')
+        const timer = setTimeout(() => {
+            //Reset error
+            setMessage({error: '', success: ''})
+        }, 4000)
+        return () => clearTimeout(timer);
+    }, [alert])
   return (
     <Box sx={{'& > *':{mb:2}, my:2}}>
-        {error && <Alert severity='error'>{error}</Alert> }
+        {(message.error || message.success) && <Alert severity={message.error ? 'error' : 'success'}>{message.error ? message.error : message.success}</Alert> }
         <TextField
             name='title'
             label='Title'
