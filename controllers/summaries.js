4f6eb007507bc393ec990b8ffad745a5a3dc3723
Tee O <tdopress@gmail.com>
2023-03-09 18:41:23 -0500

    implement functionality to create a new summary

---
+++ controllers/summaries.js
@@ -0,0 +1,28 @@
+const Summary = require("../models/Summary");
+const Document = require("../models/Document");
+
+module.exports = {
+    createSummary: async (req, res) => {
+        const {title, description, source, date, docId, link, section} = req.body;
+        try {
+            const sum = await Summary.create({ 
+                title, 
+                description, 
+                source, 
+                date, 
+                docId, 
+                link, 
+                section, 
+            });
+            //Update document lastEdited feild
+            const now = Date.now()
+            const document = await Document.findOneAndUpdate({ _id: docId }, {lastEdited: now});
+
+            console.log(`summary created`)
+            res.send({summary: sum})
+        } catch (err) {
+            console.log(err);
+            res.send({error: `Error creating summary: ${err}`})
+        }
+    },
+}
