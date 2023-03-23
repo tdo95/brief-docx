997fbe985faba6a088c0e72442bd0ea7f84f6abe
Tee O <tdopress@gmail.com>
2023-03-23 19:07:17 -0400

    migrate word and pdf generation into utilty functions

--- controllers/documents.js
+++ controllers/documents.js
@@ -1,15 +1,7 @@
 const Document = require("../models/Document");
 const Summary = require("../models/Summary");
+const { generateWordDocBuffer, generatePdfAndSend } = require( "../utilities/docCreationUtils");
 const path = require('path');
-const fs = require('fs')
-const alloTemplate = fs.readFileSync(path.join(__dirname, '../templates/allo-test.docx'), 'binary')
-const PizZip = require('pizzip')
-const Docxtemplater = require("docxtemplater");
-const LinkModule = require('docxtemplater-link-module');
-const docxConverter = require('docx-pdf');
-// const libre = require('libreoffice-convert');
-// const libreConvert = require('util').promisify(libre.convert);
-
 
 module.exports = {
   getDocuments: async (req, res) => {
@@ -81,71 +73,45 @@ module.exports = {
     else res.send({error: `${templateName} template doesnt exist`})
   },
   generateDocument: async (req, res) => {
-    const templateName = req.params.template.toLowerCase()
+    const { template, docType, docId } = req.params
     
-    if (templateName === 'allogene') {
-      //get document info from database
-      const docInfo = await Document.findById({_id: req.params.docId})
-      //get all summaries associated with the document sorted by date
-      const summaries = await Summary.find({docId: req.params.docId}).sort({date: "desc"})
-      //format summaries into section catagories and format links to be compatible with docxtemplater link module
-      const docData = summaries.reduce((obj, item) => {
-        if (!obj[item.section]) obj[item.section] = [];
-        //add formatted summary to section
-        obj[item.section].push({
-          description: item.description,
-          source: item.source,
-          date: item.date.toLocaleDateString('en-US'),
-          link: {
-            text: item.title,
-            url: item.link
-          }
-        })
-        return obj
-      }, {})
+    if (template.toLowerCase() === 'allogene') {
+      try {
+        //get document info from database
+        const docInfo = await Document.findById({_id: docId})
+        //get all summaries associated with the document sorted by date
+        const summaries = await Summary.find({docId: req.params.docId}).sort({date: "desc"})
+        //format summaries into section catagories and format links to be compatible with docxtemplater link module
+        const docData = summaries.reduce((obj, item) => {
+          if (!obj[item.section]) obj[item.section] = [];
+          //add formatted summary to section
+          obj[item.section].push({
+            description: item.description,
+            source: item.source,
+            date: item.date.toLocaleDateString('en-US'),
+            link: {
+              text: item.title,
+              url: item.link
+            }
+          })
+          return obj
+        }, {})
+        //add document creation date formatted Month, Day, Year
+        docData.date = docInfo.creationDate.toLocaleString(undefined, {
+          year: 'numeric',
+          month: 'long',
+          day: 'numeric'
+        }).toString()
 
-      //add document creation date formatted Month, Day, Year
-      docData.date = docInfo.creationDate.toLocaleString(undefined, {
-        year: 'numeric',
-        month: 'long',
-        day: 'numeric'
-      }).toString()
-
-      const linkModule = new LinkModule()
-      const zip = new PizZip(alloTemplate)
-      const doc = new Docxtemplater(zip)
-        doc.attachModule(linkModule)
-        doc.setData(docData)
-        doc.render()
-
-      const docxBuf = doc.getZip().generate({
-        type:"nodebuffer"
-      });
-      // Note: I Would prefer to use libreoffice Convert library as it produces a more accurately formatted PDF result however, this library has libre office as a software dependency which I wouldn't be able to upload on the server when I use a free hosting platform. Also, there is A LOT of latency running the convert function(17,000 ms!! for event handler to complete) part of which I am guessing might derive from time wasted opening and closing the service on my computer as suggested in https://stackoverflow.com/questions/61447666/headless-libreoffice-very-slow-to-export-to-pdf-on-windows-6-times-slow-than-on post. For now I will be using the docx-pdf library even though it is old/not maintained and requires that files be downloaded on to the disk
-  
-      // let pdfBuf = await libreConvert(docxBuf, '.pdf', undefined);
-      // res.send(pdfBuf);
-      
-      //Note: This option requires that files be saved onto the disk which presents an issue in the case where the event handler is run concurrently (2 requests executed at the same time). I might need to implement some sort of locking method to prevent unexpected behavior. For now I will opt to use the method rewriting into these output files defined below. Docx-pdf uses mammoth under the hood which converts docx to html to pdf and doesnt retain the structure (https://stackoverflow.com/questions/53586207/how-convert-word-document-to-pdf-in-nodejs) I am sacrificing proper formatting with this solution. In the future I might want to consider renaming these files to be unique but then that presents the issue of cluttering the disk space. I'll need to look more deeply into this later if the scope of this project expands.
-      
-      const inpath = path.join(__dirname, '../tmp/outdocx.docx');
-      const outpath = path.join(__dirname, '../tmp/outpdf.pdf');
-      fs.writeFileSync(inpath, docxBuf)
-      //Note: this function logs a random array to the console... not sure what that's about lol
-      docxConverter(inpath,outpath, function(err,result){
-        if(err){
-          return console.log(err);
-        }
-        console.log('PDF generated')
-        console.log(outpath)
-        res.contentType("application/pdf");
-        res.sendFile(outpath);
-      });
-      
+        const docxBuf = generateWordDocBuffer(docData)
+        if (docType === 'word') {
+          res.type('application/vnd.openxmlformats-officedocument.wordprocessingml.document')
+          res.send(docxBuf)
+        } else generatePdfAndSend(docxBuf, res)
+      } catch (err) {
+        res.send({error:`Couldn't generate document: ${err}`})
+      }
     }
     else res.send({error: `${templateName} template doesnt exist`})
-  },
-  generatePDF: async (req, res) => {
-    //TODO: Reduce code duplication, purhaps move part of this code into a helper function
   },
 };
