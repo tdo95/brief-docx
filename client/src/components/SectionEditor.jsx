a92ae8c049176dac7904783d3051030b8db95abf
Tee O <tdopress@gmail.com>
2023-03-25 16:43:28 -0400

    dynamically generate tab panels based on existing summary sections

--- client/src/components/SectionEditor.jsx
+++ client/src/components/SectionEditor.jsx
@@ -2,34 +2,47 @@ import { React, useState } from 'react'
 import { Tabs, Tab, Box } from '@mui/material'
 import TabPanel from './TabPanel'
 import SectionPage from './SectionPage'
+import { useDocument } from './context/document'
+import { useEffect } from 'react'
 
 const SectionEditor = ({setChangeInSummaries, summaries}) => {
-  
+  const document = useDocument();
+  const formatSections = () => {
+    const list = document.editing.template.toLowerCase() === 'allogene' ? ['corp', 'competitor', 'industry', 'opinion'] : [];
+    
+    return summaries.reduce((section, summary) => {
+      if (!list.includes(summary.section)) list.push(summary.section);
+      if (!section[summary.section])section[summary.section] = [];
+      section[summary.section].push(summary);
+      return section;
+    }, {list})
+  }
   const [value, setValue] = useState(0);
+  const [sections, setSections] = useState({})
   const handleChange = (event, newValue) => {
     setValue(newValue)
   }
-  
+  useEffect(() => {
+    const newSections = formatSections()
+    setSections(newSections)
+  }, [summaries])
+
   return (
     <Box>
       <Box>
-        {/* TODO: Dynamically generate tabs and tab panels */}
+        {/* Dynamically generate tabs and tab panels */}
         <Tabs value={value} onChange={handleChange}>
-          <Tab label='corp'></Tab>
-          <Tab label='competitor'></Tab>
-          <Tab label='industry'></Tab>
-          <Tab label='opinion'></Tab>
+          {sections.list?.map((label,i) => <Tab key={i} label={label}/>)}
         </Tabs>
-        <TabPanel value={value} index={0}>
-          <SectionPage 
-            sectionName={'corp'}
-            setChangeInSummaries={setChangeInSummaries} 
-            summaries={summaries} 
-          />
-        </TabPanel>
-        <TabPanel value={value} index={1}>Competitor</TabPanel>
-        <TabPanel value={value} index={2}>Industry</TabPanel>
-        <TabPanel value={value} index={3}>Opinion</TabPanel>
+        {sections.list?.map((label,i) => 
+          <TabPanel value={value} index={i}>
+            <SectionPage 
+              sectionName={label}
+              setChangeInSummaries={setChangeInSummaries}
+              summaries={(sections[label] || [])}
+            />
+          </TabPanel>
+        )}
       </Box>
 
     </Box>
