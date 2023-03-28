3947eec874ebba528d80c4b155794f90b19ee5f9
Tee O <tdopress@gmail.com>
2023-03-28 15:05:33 -0400

    implement ability to add sections for notes documents

--- client/src/components/SectionEditor.jsx
+++ client/src/components/SectionEditor.jsx
@@ -1,14 +1,14 @@
-import { React, useState } from 'react'
-import { Tabs, Tab, Box, Typography, Button, TextField } from '@mui/material'
+import { React, useState, useEffect } from 'react'
+import { Tabs, Tab, Box, Typography, Button, TextField, IconButton } from '@mui/material'
 import TabPanel from './TabPanel'
 import SectionPage from './SectionPage'
 import { useDocument } from './context/document'
-import { useEffect } from 'react'
+import CloseIcon from '@mui/icons-material/Close';
 
 const SectionEditor = ({setChangeInSummaries, summaries}) => {
   const document = useDocument();
   const formatSections = () => {
-    const list = document.editing.template.toLowerCase() === 'allogene' ? ['corp', 'competitor', 'industry', 'opinion'] : [];
+    const list = document.editing.template.toLowerCase() === 'allogene' ? ['corp', 'competitor', 'industry', 'opinion'] : sections.list;
     
     return summaries.reduce((section, summary) => {
       if (!section.list.includes(summary.section)) section.list.push(summary.section);
@@ -18,8 +18,12 @@ const SectionEditor = ({setChangeInSummaries, summaries}) => {
     }, {list})
   }
   const [value, setValue] = useState(0);
-  const [sections, setSections] = useState({})
+  const [sections, setSections] = useState({list: []})
   const [openForm, setOpenForm] = useState(false)
+  const [formError, setFormError] = useState('')
+  const [form, setForm] = useState({
+    sectionTitle: ''
+  })
   const handleChange = (event, newValue) => {
     setValue(newValue)
   }
@@ -28,19 +32,68 @@ const SectionEditor = ({setChangeInSummaries, summaries}) => {
     setSections(newSections)
     console.log('SECTIONS',sections)
   }, [summaries])
-  const addSection = () => {
-    //open form to enter section name
-    setOpenForm(prev => !prev)
+  const handleForm = (e) => {
+    const { name, value } = e.target
+
+    setForm(prev => ({
+      ...prev,
+      [name]:value,
+    }))
   }
-  console.log('document:', document.editing)
+  const addSection = () => {
+
+    if (openForm) {
+      const title = form.sectionTitle.toLowerCase().trim()
+      if (!title.length) return setFormError('Please enter a title')
+      if (sections.list.includes(title)) return setFormError('This title already exists')
+      //save form name
+      sections.list.push(title)
+      //trigger change to update
+      setChangeInSummaries(prev => !prev)
+      //close form
+      setOpenForm(false)
+    }
+    else {
+      //open form to enter section name
+      setOpenForm(true)
+    }
+    //reset form errors
+    setFormError('')
+    //clear section title
+    setForm(prev => ({
+      ...prev,
+      sectionTitle: ''
+    }))
+  }
+
   return (
     <Box>
       {
         document.editing.template.toLowerCase() !== 'allogene' && 
           <Box>
             { !sections.list?.length && <Typography>Looks like you dont have any note sections yet. Add one below!</Typography>}
-            <Button variant='contained' onClick={addSection}>Add Section</Button>
-            {openForm && <TextField />}
+            <Box sx={{display:'flex', gap: '10px', alignItems:'start'}}>
+              {
+                openForm &&
+                  <>
+                    <IconButton onClick={() => setOpenForm(false)}>
+                      <CloseIcon />
+                    </IconButton>
+                    <TextField
+                      name='sectionTitle'
+                      label='Section Title'
+                      placeholder='Enter section title'
+                      size='small'
+                      value={form.sectionTitle}
+                      onChange={handleForm}
+                      helperText={formError}
+                      error={!!formError}
+                    />
+                  </>
+              }
+              <Button variant='contained' onClick={addSection}>{ openForm ? 'Save' : 'Add Section'}</Button>
+            </Box>
+            
           </Box>
       }
       {
@@ -48,10 +101,10 @@ const SectionEditor = ({setChangeInSummaries, summaries}) => {
           <Box>
             {/* Dynamically generate tabs and tab panels */}
             <Tabs value={value} onChange={handleChange}>
-              {sections.list?.map((label,i) => <Tab key={i} label={label}/>)}
+              {sections.list?.map((label,i) => <Tab key={i} label={label.length > 15 ? label.slice(0,15) + '...' : label}/>)}
             </Tabs>
             {sections.list?.map((label,i) => 
-              <TabPanel value={value} index={i}>
+              <TabPanel key={i} value={value} index={i}>
                 <SectionPage 
                   sectionName={label}
                   setChangeInSummaries={setChangeInSummaries}
