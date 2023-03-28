c3d6619da19285da127f392df0a0b90363a57738
Tee O <tdopress@gmail.com>
2023-03-28 17:14:21 -0400

    change error state structure and add duplication warning

--- client/src/components/SummaryForm.jsx
+++ client/src/components/SummaryForm.jsx
@@ -7,8 +7,8 @@ const Form = ({ section, lastEnteredDate, setLastEnteredDate, setChangeInSummari
     const document = useDocument();
     const [alert, setAlert] = useState(false)
     const [message, setMessage] = useState({
-        error: '',
-        success: ''
+        type: '',
+        message: ''
     })
     const [form, setForm] = useState({
         title: summaryData.title || '',
@@ -22,7 +22,7 @@ const Form = ({ section, lastEnteredDate, setLastEnteredDate, setChangeInSummari
         const { name, value } = e.target;
         setForm(prev => {
             const newForm = {...prev}
-            newForm[name] = value.trim();
+            newForm[name] = value
             if(name === 'date'){ 
                 let date = value.split('-')
                 date.push(date.shift())
@@ -37,20 +37,37 @@ const Form = ({ section, lastEnteredDate, setLastEnteredDate, setChangeInSummari
             return newForm
         })
     }
+    const cleanText = () => {
+        //replace parenthesis
+        const parenReg = /(\(.*?\))/gi;
+        //replace spaces
+        const spaceReg = /  +/g;
+        console.log('cleaning text')
+        //remove parenthesis inside of text
+        setForm(prev => ({
+            ...prev,
+            description: form.description.replace(parenReg, '').replace(spaceReg, ' ')
+        }))
+    }
+    const showFeedback = (type, message) => {
+        //Note: Im using the alert state variable as a trigger for the use effect to clear the alert after a few seconds, there it probably a better way to go about this, but this will do for now.
+        setAlert(prev => !prev)
+        setMessage({type: type, message: message })
+    }
     const saveForm = async () => {
         if (document.editing.template === 'Allogene') {
             //Check if any form values are empty
             if(Object.values(form).some(value => !value)){
                 //Note: Im using the alert state variable as a trigger for the use effect to clear the alert after a few seconds, there it probably a better way to go about this, but this will do for now.
                 setAlert(prev => !prev)
-                setMessage({success: '', error: 'Please fill out each feild in the form' })
+                setMessage({type: 'error', message: 'Please fill out each feild in the form' })
                 return
             }
         
         } else if (document.editing.template === 'Notes') {
             if(!form.description.length || !form.title.length) {
                 setAlert(prev => !prev)
-                setMessage({success: '', error: 'Please enter a description and title.' })
+                setMessage({type: 'error', message: 'Please enter a description and title.' })
                 return
 
             }
@@ -59,7 +76,7 @@ const Form = ({ section, lastEnteredDate, setLastEnteredDate, setChangeInSummari
         //Check that link adheres to 'https://' format
         if (form.link && !isValidHttpUrl(form.link)) {
             setAlert(prev => !prev)
-            setMessage({success: '', error: 'Please enter your link in "https://" format' })
+            setMessage({type: 'error', message: 'Please enter your link in "https://" format' })
             return
         }
 
@@ -75,11 +92,11 @@ const Form = ({ section, lastEnteredDate, setLastEnteredDate, setChangeInSummari
             })
             if (res.error) {
                 setAlert(prev => !prev)
-                setMessage({success: '', error: 'Opps. An error occured while trying to save your summary, please try again later.' })
+                setMessage({type: 'error', message: 'Opps. An error occured while trying to save your summary, please try again later.' })
 
             } else {
                 setAlert(prev => !prev)
-                setMessage({error: '', success: 'Success! Summary has been updated' })
+                setMessage({type: 'success', message: 'Success! Summary has been updated' })
 
                 setChangeInSummaries(prev => !prev)
             }
@@ -95,13 +112,18 @@ const Form = ({ section, lastEnteredDate, setLastEnteredDate, setChangeInSummari
                 link: form.link,
                 section: section,
             })
-            if (res.error) {
+            if (res.error ) {
                 setAlert(prev => !prev)
-                setMessage({success: '', error: 'Opps. An error occured while trying to save your summary, please try again later.' })
+                setMessage({type: 'error', message: 'Opps. An error occured while trying to save your summary, please try again later.' })
+            }
+            else if (res.warning) {
+                setAlert(prev => !prev)
+                setMessage({type: 'warning', message: `Warning: "${res.warning.document.toUpperCase()}" created on ${res.warning.date.toUpperCase()} references this link.`})
+                setChangeInSummaries(prev => !prev)
             }
             else if (res.summary) {
                 setAlert(prev => !prev)
-                setMessage({error: '', success: 'Success! Summary has been added' })
+                setMessage({type: 'success', message: 'Success! Summary has been added' })
                 //clear form
                 setForm(prev => ({
                     ...prev,
@@ -118,13 +140,13 @@ const Form = ({ section, lastEnteredDate, setLastEnteredDate, setChangeInSummari
         console.log('Form Alert Timer going boi')
         const timer = setTimeout(() => {
             //Reset error
-            setMessage({error: '', success: ''})
+            setMessage({type: '', message: ''})
         }, 4000)
         return () => clearTimeout(timer);
     }, [alert])
   return (
     <Box sx={{'& > *':{mb:'24px'}, my:2}}>
-        {(message.error || message.success) && <Alert severity={message.error ? 'error' : 'success'}>{message.error ? message.error : message.success}</Alert> }
+        {(message.type) && <Alert severity={message.type}>{message.message}</Alert> }
         <TextField
             name='title'
             label='Title'
@@ -140,6 +162,9 @@ const Form = ({ section, lastEnteredDate, setLastEnteredDate, setChangeInSummari
             onChange={handleForm} 
             multiline
             fullWidth
+            inputProps={{
+                spellcheck: true
+            }}
         />
         { document.editing.template === 'Allogene' && 
             <><TextField
@@ -170,6 +195,7 @@ const Form = ({ section, lastEnteredDate, setLastEnteredDate, setChangeInSummari
                 value={form.date}  
             /></>
         }
+        {document.editing.template === 'Allogene' &&  <Button sx={{mr:2}} variant='outlined' onClick={cleanText}>Clean Text</Button>}
         <Button sx={{ml:'auto'}} variant='contained' onClick={saveForm}>Save</Button>
     </Box>
   )
