dc01afe2926323586ecb207c473fa4abe1780e86
Tee O <tdopress@gmail.com>
2023-03-28 17:11:55 -0400

    render different add button label base on document template

--- client/src/components/SectionPage.jsx
+++ client/src/components/SectionPage.jsx
@@ -2,8 +2,10 @@ import {React, useState} from 'react'
 import { Button } from '@mui/material'
 import Form from './SummaryForm'
 import SummaryItem from './SummaryItem'
+import { useDocument } from './context/document'
 const SectionPage = ({sectionName, summaries, setChangeInSummaries, sectionsList}) => {
     const today = new Date();
+    const document = useDocument()
     const [toggleForm, setToggleForm] = useState(false)
     const [lastEnteredDate, setLastEnteredDate] = useState({regular: today.toLocaleDateString('en-CA'), formatted: today.toLocaleDateString('en-US')})
   return (
@@ -12,7 +14,7 @@ const SectionPage = ({sectionName, summaries, setChangeInSummaries, sectionsList
           variant='contained' 
           onClick={() => setToggleForm(prev => !prev)}
         >       
-          {toggleForm ? 'X' : 'Add Summary'}
+          {toggleForm ? 'X' : document.editing.template === 'Allogene' ? 'Add Summary' : 'Add Note'}
         </Button>
         { toggleForm && 
           <Form 
