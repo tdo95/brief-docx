e76d29f4643e61a0967d8828b2700c49693503ce
Tee O <tdopress@gmail.com>
2023-03-30 17:52:22 -0400

    add description content edit

--- client/src/pages/Product.jsx
+++ client/src/pages/Product.jsx
@@ -14,7 +14,7 @@ const Product = () => {
               <Box sx={{width:'50%', height: '300px', overflow:'hidden', position:'relative'}}> <img style={{width:'100%', position: 'absolute', top:'-110px'}} src={float}/> </Box>
               <Box sx={{width:'60%', display:'flex', flexDirection:'column', justifyContent: 'center'}}>
                 <Typography sx={{fontSize: '22px', fontWeight:'500', mb: '10px'}}>Need to generate a custom document, but short on time?</Typography>
-                <Typography sx={{fontSize: '20px', mb:'20px'}}>Breif Docx is a document formating tool that allows you to edit your documents in real time. Formatting is taken care of behind the scenes. This way you can focus on what really matters - the content!</Typography>
+                <Typography sx={{fontSize: '20px', mb:'20px'}}>Breif Docx is a document formating tool that allows you to create and edit your documents in real time. Formatting is taken care of behind the scenes. This way you can focus on what really matters, the content!</Typography>
                 <Stack direction='row' spacing={3}>
                 <Button component={ Link } to='/about' variant='outlined'>Learn More</Button>
                 <Button component={ Link } to='/login' variant='contained'>Get Started</Button>
