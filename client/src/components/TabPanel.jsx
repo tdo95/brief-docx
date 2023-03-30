fc37707ca6de44cd4ba0a8c57e1fc8853cf397aa
Tee O <tdopress@gmail.com>
2023-03-30 15:11:17 -0400

    modify style

--- client/src/components/TabPanel.jsx
+++ client/src/components/TabPanel.jsx
@@ -3,12 +3,13 @@ import { Box, Typography } from '@mui/material'
 const TabPanel = ({children, value, index}) => {
   return (
     <div
+        style={{borderTop: 'solid 1px lightgray'}}
         role={'tabpanel'}
         hidden={value !==index}
         aria-labelledby={`section-tab-${index}`}
     >
-        <Box sx={{p:4}}>
-            <Box>{children}</Box>
+        <Box sx={{pt:4, pl:4,pb:4, pr: 0,  }}>
+            <Box >{children}</Box>
         </Box>
     </div>
   )
