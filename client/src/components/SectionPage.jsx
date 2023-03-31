c42b09094c97d5c1d79a1c57a5cca108ad665835
Tee O <tdopress@gmail.com>
2023-03-31 10:36:20 -0400

    add container overflow styling

--- client/src/components/SectionPage.jsx
+++ client/src/components/SectionPage.jsx
@@ -15,9 +15,9 @@ const SectionPage = ({sectionName, summaries, setRefreshDocument, sectionsList})
     const lessThan500 = useMediaQuery('(max-width:600px)')
 
   return (
-    <div>
-        <Box sx={{display: (greaterThan900 || lessThan500) ?'block' : 'flex', gap: '20px', height: (greaterThan900 || lessThan500) ? '550px': '400px', overflowY: (greaterThan900 || lessThan500) ?'scroll' : 'none', width: '100%'}}>
-          <Stack sx={{width:(greaterThan900 || lessThan500) ? '95%' : '55%', height: !toggleForm ? '50px' : '300px', }}>
+    <>
+        <Box sx={{display: (greaterThan900 || lessThan500) ?'block' : 'flex', gap: '20px', height: (greaterThan900 || lessThan500) ? '550px': '400px', width: '100%'}}>
+          <Stack sx={{width:(greaterThan900 || lessThan500) ? '95%' : '55%',  overflowY: 'auto', maxHeight:(greaterThan900 || lessThan500) ? document.editing.template === 'work' ? '450px' : '290px' : 'unset', mb:'20px' }}>
             <Button
               variant='contained'
               onClick={() => setToggleForm(prev => !prev)}
@@ -33,13 +33,13 @@ const SectionPage = ({sectionName, summaries, setRefreshDocument, sectionsList})
               />
             }
           </Stack>
-          <Stack className='scroll' sx={{width:(greaterThan900 || lessThan500) ? '90%' : '50%', overflowY: 'scroll', border:'1px solid lightgray', py: '5px', px: '15px', borderRadius:'10px', height: '400px' }}>
+          <Stack className='scroll' sx={{width:(greaterThan900 || lessThan500) ? '90%' : '50%', overflowY: 'auto', border:'1px solid lightgray', py: '5px', px: '15px', borderRadius:'10px', maxHeight:(greaterThan900 || lessThan500) ? document.editing.template === 'work' ? '200px' : '250px' : 'unset' }}>
             {summaries.map((item,i) => <SummaryItem key={i} summaryData={item} setRefreshDocument={setRefreshDocument}
             setLastEnteredDate={setLastEnteredDate}
             lastEnteredDate={lastEnteredDate} sectionsList={sectionsList.filter(name => name !== sectionName)}/>)}
           </Stack>
         </Box>
-    </div>
+    </>
   )
 }
 
